0bfaa5e88a60305a3e61e748387df958880b76cc
leejunho0622 <lee49409780@gmail.com>
2024-11-14 16:59:28 +0900

    Add StageTitle class and @override start()

--- testrpg/src/testrpg/StageTitle.java
+++ testrpg/src/testrpg/StageTitle.java
@@ -1,5 +1,25 @@
 package testrpg;
 
+import java.io.IOException;
+
 public class StageTitle extends Stage{
+	@Override
+	public void init() {}
+
+	@Override
+	public void start() {
+		try {
+			String textTitle = String.format("======== TEXT RPG ========\n[시작]을 입력하세요.\n");
+			IOManager.append(textTitle);
+			
+			String input = reader.readLine();
+			
+			if(input.equals("시작"))
+				GameManager.nextStage = "LOBBY";
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
