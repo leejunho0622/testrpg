f1f83e9ec69cb557ed7247dde096dcaf4bac5406
leejunho0622 <lee49409780@gmail.com>
2024-11-14 17:09:51 +0900

    Modify: textTitle String format

--- testrpg/src/testrpg/StageTitle.java
+++ testrpg/src/testrpg/StageTitle.java
@@ -9,17 +9,16 @@ public class StageTitle extends Stage{
 	@Override
 	public void start() {
 		try {
-			String textTitle = String.format("======== TEXT RPG ========\n[시작]을 입력하세요.\n");
+			String textTitle = String.format("======== TEXT RPG ========\n[시작]을 입력하세요\n");
 			IOManager.append(textTitle);
-			
+
 			String input = reader.readLine();
-			
-			if(input.equals("시작"))
+
+			if (input.equals("시작"))
 				GameManager.nextStage = "LOBBY";
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
