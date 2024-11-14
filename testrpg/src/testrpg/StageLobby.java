1e552cdcc0588b9004c8bf9abfc0db4efb141df5
leejunho0622 <lee49409780@gmail.com>
2024-11-14 17:08:42 +0900

    Add StageLobby class and @Override start()

--- testrpg/src/testrpg/StageLobby.java
+++ testrpg/src/testrpg/StageLobby.java
@@ -1,5 +1,27 @@
 package testrpg;
 
-public class StageLobby extends Stage{
+public class StageLobby extends Stage {
+	@Override
+	public void init() {}
 
+	@Override
+	public void start() {
+		try {
+			String textTitle = String.format("============ LOBBY ============\n   [1] 전투  [2] 설정  [3] 종료\n");
+			IOManager.append(textTitle);
+
+			String input = reader.readLine();
+			int select = Integer.parseInt(input);
+
+			if (select == 1)
+				GameManager.nextStage = "BATTLE";
+			else if (select == 2)
+				GameManager.nextStage = "SETTING";
+			else if (select == 3)
+				GameManager.gamePlay = false;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
