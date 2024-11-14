e03db4aa08a8e17292b0423c73c7385947b5e6d9
leejunho0622 <lee49409780@gmail.com>
2024-11-14 15:39:46 +0900

    Update init(), Add run()

--- testrpg/src/testrpg/GameManager.java
+++ testrpg/src/testrpg/GameManager.java
@@ -1,7 +1,14 @@
 package testrpg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameManager {
 	private static GameManager instance = new GameManager();
+	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	
+	private boolean gamePlay = true;
 	
 	private GameManager() {}
 	
@@ -9,5 +16,18 @@ public class GameManager {
 		return instance;
 	}
 	
-	public void init() {}
+	public void init() { // 초기화 함수
+		stageList.put("TITLE", new StageTitle());
+		stageList.put("LOBBY", new StageLobby());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("SETTING", new StageSetting());
+	} 
+	
+	
+	public void run() { // 게임 실행 메소드
+		while(gamePlay) {
+			
+		}
+		
+	}
 }
