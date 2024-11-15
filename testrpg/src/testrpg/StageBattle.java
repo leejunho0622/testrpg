cd92e81e213c5687ab15cb1825d61b65b4540e21
leejunho0622 <lee49409780@gmail.com>
2024-11-15 15:52:45 +0900

    Add printDashBoard()

--- testrpg/src/testrpg/StageBattle.java
+++ testrpg/src/testrpg/StageBattle.java
@@ -1,9 +1,29 @@
 package testrpg;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class StageBattle extends Stage{
+	
+	private Random ran = new Random();
+	
 	@Override
 	public void init() {}
+	
 
+	
+	private void printDashBoard() {
+		System.out.println("====== BATTLE ======");
+		System.out.println("====== PLAYER ======");
+		for (int i = 0; i < Guild.getPartyCount(); i++) {
+			Guild.getPartyPlayer(i);
+		}
+		System.out.println("====== MONSTER ======");
+		for (int i = 0; i < MonsterManager.getMonsterListSize(); i++) {
+			MonsterManager.getMonster(i);
+		}
+	}
+	
 	@Override
 	public void start() {
 		
