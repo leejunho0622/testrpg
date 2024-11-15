dbb2a20ca0dfe926a142ed12bf942928eef65432
leejunho0622 <lee49409780@gmail.com>
2024-11-15 18:27:32 +0900

    Add selectAttack() and print message, Update start()

--- testrpg/src/testrpg/StageBattle.java
+++ testrpg/src/testrpg/StageBattle.java
@@ -1,6 +1,5 @@
 package testrpg;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class StageBattle extends Stage{
@@ -10,22 +9,52 @@ public class StageBattle extends Stage{
 	@Override
 	public void init() {}
 	
-
-	
 	private void printDashBoard() {
 		System.out.println("====== BATTLE ======");
 		System.out.println("====== PLAYER ======");
 		for (int i = 0; i < Guild.getPartyCount(); i++) {
-			Guild.getPartyPlayer(i);
+			IOManager.append(Guild.getPartyPlayer(i).toString());
 		}
 		System.out.println("====== MONSTER ======");
 		for (int i = 0; i < MonsterManager.getMonsterListSize(); i++) {
-			MonsterManager.getMonster(i);
+			IOManager.append(MonsterManager.getMonster(i).toString());
 		}
+	}
+	
+	private boolean selectAttack() {
+		for(int i=0; i<Guild.getPartyCount(); i++) {
+			Player user = Guild.getPartyPlayer(i);
+			String selectMenu = String.format("[%s:%s] [1. 공격][2. 스킬]", user.getName(), user.getjob());
+			IOManager.append(selectMenu);
+			try {
+				String input = reader.readLine();
+				int select = Integer.parseInt(input);
+				
+				if(select == 1) {
+					int rNum = ran.nextInt(MonsterManager.getMonsterListSize());
+					user.attack(MonsterManager.getMonster(rNum));
+				}
+				else if(select == 2) {
+					Skill.useSkill(user);
+				}
+					
+			} catch (Exception e) {
+			}
+			
+		}
+		return true;
+		
 	}
 	
 	@Override
 	public void start() {
+		Guild.createParty();
+		MonsterManager.makeMonsters();
+		printDashBoard();
+		while(true) {
+			if(selectAttack())
+				break;
+		}
 		
 	}
 }
