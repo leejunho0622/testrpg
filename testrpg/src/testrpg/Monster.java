bcb7982856cab16f969fd4b836551b544f8b54df
leejunho0622 <lee49409780@gmail.com>
2024-11-15 15:53:44 +0900

    Add monster fields

--- testrpg/src/testrpg/Monster.java
+++ testrpg/src/testrpg/Monster.java
@@ -1,5 +1,15 @@
 package testrpg;
 
-public class Monster {
-
+public abstract class Monster {
+	String name;
+	int hp;
+	int maxHp;
+	int damage;
+	
+	public Monster(String name, int hp, int maxHp, int damage) {
+		this.name = name;
+		this.hp = hp;
+		this.maxHp = maxHp;
+		this.damage = damage;
+	}
 }
