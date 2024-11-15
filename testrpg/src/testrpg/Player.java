31e4b87a3e2ce33e25518c364fb0e869e0f79dea
leejunho0622 <lee49409780@gmail.com>
2024-11-15 18:28:08 +0900

    Delete: \n

--- testrpg/src/testrpg/Player.java
+++ testrpg/src/testrpg/Player.java
@@ -2,9 +2,8 @@ package testrpg;
 
 public class Player extends Unit{
 	public static int money;
-	
+
 	public Player(String name, String job, int level, int hp, int mp, int damage, int def, int exp) {
 		super(name, job, level, hp, mp, damage, def, exp);
 	}
-
 }
